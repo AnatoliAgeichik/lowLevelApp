b0a0e68c5e968954e63dd9a30842da45f2ecb565
AnatoliAgeichik <3anatoliageichik@gmail.com>
2023-09-02 18:19:53 +0300

    config pre-commit

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import socket
 
 URLS = {"/": "Hello, World", "/team": "Hello, Team"}
 
+
 def get_body(status: int, url: str):
     if status == 200:
-        return f'<h1>{URLS.get(url)}</h1>'
+        return f"<h1>{URLS.get(url)}</h1>"
     elif status == 404:
         return f"<h1>404</h1><p>Not found</p>"
     elif status == 405:
         return f"<h1>405</h1><p>Method not allowed</p>"
 
-def parse_method_and_url_from_request(request:str) -> tuple[str]:
+
+def parse_method_and_url_from_request(request: str) -> tuple[str]:
     request_list = request.split()
     return (request_list[0].decode(), request_list[1].decode())
+
 
 def get_status(method: str, url: str) -> int:
     if url not in URLS.keys():
@@ -22,21 +25,22 @@ def get_status(method: str, url: str) -> int:
         return 405
     return 200
 
+
 def get_header(status: int) -> tuple:
     if status == 200:
-        return 'HTTP/1.0 200 OK\n\n'
+        return "HTTP/1.0 200 OK\n\n"
     elif status == 404:
         return "HTTP/1.1 404 Not found\n\n"
     elif status == 405:
         return "HTTP/1.1 405 Method not allowed\n\n"
 
-def generate_response(request:str):
+
+def generate_response(request: str):
     method, url = parse_method_and_url_from_request(request)
     status = get_status(method, url)
     header = get_header(status)
     body = get_body(status, url)
     return (header + body).encode()
-
 
 
 def run():
@@ -50,8 +54,6 @@ def run():
         request = client_socket.recv(1024)
         client_socket.sendall(generate_response(request))
         client_socket.close()
-
-
 
 
 if __name__ == "__main__":
